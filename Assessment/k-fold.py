fa08f19ded9705d23f2723519d938ab0c00fb31e
rnmourao <robertonunesmourao@yahoo.com.br>
2019-10-22 22:33:02 -0300

    new tests

--- Assessment/k-fold.py
+++ Assessment/k-fold.py
@@ -64,11 +64,13 @@ def solution(indices, K):
     
 
 if __name__ == '__main__':
+    indices, K = ([1,2,3,4], 3)
+
     # indices, K = ([1, 2], 2)
 
     # indices, K = ([1,2,3], 2)
 
-    indices, K = ([1,2,3,4,5,6,7,8,9,10], 3)
+    # indices, K = ([1,2,3,4,5,6,7,8,9,10], 3)
 
     # indices, K = ([1,2,3,4,5,6,7,8,9], 3)
 
