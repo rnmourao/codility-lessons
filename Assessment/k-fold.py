5d599c417f43b28278ec89080d6a07a406f4d330
rnmourao <robertonunesmourao@yahoo.com.br>
2019-10-23 06:03:37 -0300

    cyclic rotation

--- Assessment/k-fold.py
+++ Assessment/k-fold.py
@@ -61,10 +61,51 @@ def solution(indices, K):
         ls.append(val)
 
     return(ls)
+
+
+@timetest
+def solution2(indices, K):
+    
+    # get length of list of indices
+    lenI = len(indices)
+    
+    # get probable size of each fold
+    avg = lenI // K
+
+    # get the remainder indices of a perfect
+    rem = lenI % K
+
+    # identify folds indexes
+    ixs = []
+    i = 0
+    while i < lenI:
+        ixs.append(i)        
+        i += avg
+        if rem > 0:
+            i += 1
+            rem -= 1
+
+    # create folds
+    folds = []
+    lenIXS = len(ixs)
+    for f in range(lenIXS):
+        if f + 1 == lenIXS:
+            fold = indices[ixs[f]:]
+        else:
+            fold = indices[ixs[f]:ixs[f+1]]
+        folds.append(fold)
+
+    # create train and val sequences
+    ls = []
+    for f in range(len(folds)):
+        folds = [folds[-1]] + folds[0:-1] 
+        ls += folds
+
+    return(ls)    
     
 
 if __name__ == '__main__':
-    indices, K = ([1,2,3,4], 3)
+    # indices, K = ([1,2,3,4], 3)
 
     # indices, K = ([1, 2], 2)
 
@@ -76,11 +117,14 @@ if __name__ == '__main__':
 
     # indices, K = (list(range(10)), 10)
 
-    print(solution(indices, K))
+    # print(solution(indices, K))
+    # print(solution2(indices, K))
 
     # indices, K = (list(range(100)), 100)
     # solution(indices, K)
+    # solution2(indices, K)
 
-    # indices, K = (list(range(100000)), 100000)
+    indices, K = (list(range(100000)), 100000)
     # solution(indices, K)
+    solution2(indices, K)
 
